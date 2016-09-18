599b899d5cba8312e905b6dd00a1770e52422ba1
Gavin Cooper <gjcooper@gmail.com>
2016-09-18 23:31:29 +1000

    Add coloured printing, reports and assoc test

--- repovisor/repovisor.py
+++ repovisor/repovisor.py
@@ -1,9 +1,11 @@
 from git import Repo
 from git.exc import InvalidGitRepositoryError
+from colorama import Fore, Style, init
 import os
 import warnings
 
 vcs_statechecker = {'git': None}
+init()
 
 
 def reposearch(*folders):
@@ -50,3 +52,33 @@ def checkGit(repo):
 def addHandlers():
     """add statechker handlers to dict"""
     vcs_statechecker['git'] = checkGit
+
+
+def ahead_behind(ref):
+    if ref['ahead']:
+        glyph = Fore.RED + '+' + str(ref['ahead']) + Style.RESET_ALL
+    else:
+        glyph = '-'
+    glyph += '/'
+    if ref['behind']:
+        glyph += Fore.RED + '-' + str(ref['behind']) + Style.RESET_ALL
+    else:
+        glyph += '-'
+    return glyph
+
+
+def print_state(*repos):
+    """Print all repo current state to terminal"""
+    for repo in repos:
+        print('Location: ', repo['folder'])
+        state = repo['state']
+        print('Modified: ', Fore.RED, state['dirty'], Style.RESET_ALL)
+        print('Untracked: ', Fore.YELLOW, '\n'.join(state['untracked']),
+              Style.RESET_ALL)
+        print('Refs:')
+        for ref in state['refcheck']:
+            ab_notifier = ''
+            if ref['upstream']:
+                ab_notifier = ahead_behind(ref)
+            print('\tName: ', ref['name'], ' Upstream: ', ref['upstream'],
+                  'Status: ', ab_notifier)
