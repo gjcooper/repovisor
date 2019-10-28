44d586fd8d9875100c782ab89e8bfd059f780ff5
gjcooper <gjcooper@gmail.com>
2019-10-28 14:03:31 +1100

    Bump version and report on bracnhes without tracking remote

--- src/repovisor/__main__.py
+++ src/repovisor/__main__.py
@@ -14,7 +14,8 @@ def main():
 @click.option('--prune', '-p', is_flag=True, help='Prune directories as soon as we reach a repository')
 @click.option('--tree', '-t', is_flag=True, help='Show repositories in a tree structure, only used if brief is also selected')
 @click.option('--hide', is_flag=True, help='Hide repositories that are clean')
-def __search_action(folders, brief, prune, tree, hide):
+@click.option('--remote', is_flag=True, help='Warn if repo has no remote')
+def __search_action(folders, brief, prune, tree, hide, remote):
     '''search through a set of folders and print status for them'''
     for repo_level, repo in rv.reposearch(*folders, prune=prune, level=0):
         view = rv.repo_view(repo, brief=brief)
@@ -23,6 +24,6 @@ def __search_action(folders, brief, prune, tree, hide):
         if hide and not repo.bare:
             if not repo.state['dirty']:
                 if not repo.state['untracked']:
-                    if all(rv.branch_uptodate(b) for b in repo.state['refcheck']):
+                    if all(rv.branch_uptodate(b, true_on_missing_origin=not remote) for b in repo.state['refcheck']):
                         continue
         print(view)
